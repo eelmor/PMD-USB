85c9e551096f0939e1bdcb8ed85857a128af4eb0
JonSandström <JonSandstrm@JON-X1G9>
2024-11-14 15:26:37 +0800

    Update DataLogger format

--- PMD/DataLogger.cs
+++ PMD/DataLogger.cs
@@ -3,36 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Globalization;
 using Microsoft.Win32;
+using System.Diagnostics.Eventing.Reader;
 
 namespace PMD
 {
-    /*public class LoggingItem
-    {
-
-        public int ParentId;
-        public int Id;
-        public bool Enabled;
-        public string Description;
-        public string DescriptionShort;
-        public string Unit;
-        public double Value;
-        public string HwinfoKey;
-
-        public LoggingItem(int id, string desc, string desc_short, string unit)
-        {
-            Id = id;
-            Description = desc;
-            DescriptionShort = desc_short;
-            Unit = unit;
-            Enabled = true;
-        }
-
-        override public string ToString()
-        {
-            return Description;
-        }
-
-    }*/
 
     public class LoggingItem
     {
@@ -59,41 +33,54 @@ namespace PMD
         private const string HWINFO_REG_KEY = "Software\\HWiNFO64\\Sensors\\Custom";
         public string DEVICE_NAME;
 
-        public List<LoggingItem> LoggingItemList;
+        public List<LoggingItem> LoggingItemList = new List<LoggingItem>();
 
-        public bool IsLogging;
+        public bool IsLogging = false;
 
-        int IdCounter;
-        string FilePath;
-        bool CsvMode;
-        bool Hwinfo;
+        int IdCounter = 0;
 
-        private CultureInfo culture_info;
+        bool WriteToTextFile;
+        bool LogToCsv;
+        bool ExportToHwinfo;
+
+        string TextFilePath;
+        string CsvFilePath;
+
+        private CultureInfo culture_info = new CultureInfo("en-US");
 
         public DataLogger(string device_name)
         {
             DEVICE_NAME = device_name;
-
-            IsLogging = false;
-            IdCounter = 0;
-            LoggingItemList = new List<LoggingItem>();
-            culture_info = new CultureInfo("en-US");
         }
 
-        public void SetHwinfo(bool hwinfo)
+        public void SetHwinfo(bool enabled)
         {
-            Hwinfo = hwinfo;
+            ExportToHwinfo = enabled;
         }
 
-        public void SetCsv(string path, bool csv)
+        public void SetTextFile(string path, bool enabled)
         {
-            FilePath = path;
-            CsvMode = csv;
+            TextFilePath = path;
+            if (string.IsNullOrEmpty(TextFilePath))
+            {
+                WriteToTextFile = false;
+            }
+            else
+            {
+                WriteToTextFile = enabled;
+            }
         }
 
-        public string GetFilePath()
+        public void SetCsv(string path, bool enabled)
         {
-            return FilePath;
+            CsvFilePath = path;
+            if (string.IsNullOrEmpty(CsvFilePath)) {
+                LogToCsv = false;
+            }
+            else
+            {
+                LogToCsv = enabled;
+            }
         }
 
         public void AddLogItem(Sensor sensor)
@@ -121,7 +108,8 @@ namespace PMD
 
         public void Start()
         {
-            if (!string.IsNullOrEmpty(FilePath) && CsvMode)
+
+            if (LogToCsv && !string.IsNullOrEmpty(CsvFilePath))
             {
                 string header_line = "Timestamp,";
                 foreach (LoggingItem logging_item in LoggingItemList)
@@ -133,10 +121,10 @@ namespace PMD
                 }
                 header_line = header_line.Substring(0, header_line.Length - 1);
                 header_line += Environment.NewLine;
-                File.WriteAllText(FilePath, header_line);
+                File.WriteAllText(CsvFilePath, header_line);
             }
 
-            if (Hwinfo)
+            if (ExportToHwinfo)
             {
                 try
                 {
@@ -144,6 +132,10 @@ namespace PMD
                     if (hwinfo_reg_key == null)
                     {
                         hwinfo_reg_key = Registry.CurrentUser.CreateSubKey(HWINFO_REG_KEY, true);
+                    } else
+                    {
+                        // Clear old entries
+                        RemoveHwinfoRegEntry();
                     }
                 }
                 catch (Exception ex)
@@ -156,66 +148,8 @@ namespace PMD
                     throw new Exception("Error accessing registry.");
                 }
 
-                try
-                {
-
-                    if (hwinfo_reg_key.OpenSubKey(DEVICE_NAME) != null)
-                    {
-                        hwinfo_reg_key.DeleteSubKeyTree("");
-                    }
-
-                    hwinfo_reg_key = hwinfo_reg_key.CreateSubKey(DEVICE_NAME);
-
-                    if (hwinfo_reg_key == null)
-                    {
-                        throw new Exception("Error accessing registry.");
-                    }
-
-                    int temp_idx = 0;
-                    int current_idx = 0;
-                    int volt_idx = 0;
-                    int power_idx = 0;
-                    int fan_idx = 0;
-                    int usage_idx = 0;
-                    foreach (LoggingItem logging_item in LoggingItemList)
-                    {
-
-                        switch (logging_item.Sensor.Unit)
-                        {
-                            case "°C":
-                                logging_item.HwinfoKey = $"Temp{temp_idx++}"; break;
-                            case "A":
-                                logging_item.HwinfoKey = $"Current{current_idx++}"; break;
-                            case "V":
-                                logging_item.HwinfoKey = $"Volt{volt_idx++}"; break;
-                            case "W":
-                                logging_item.HwinfoKey = $"Power{power_idx++}"; break;
-                            case "RPM":
-                                logging_item.HwinfoKey = $"Fan{fan_idx++}"; break;
-                            case "%":
-                                logging_item.HwinfoKey = $"Usage{usage_idx++}"; break;
-                            default:
-                                logging_item.Enabled = false; break;
-                        }
-
-                        if (logging_item.Enabled)
-                        {
-                            RegistryKey key = hwinfo_reg_key.CreateSubKey(logging_item.HwinfoKey);
-                            if (key == null)
-                            {
-                                throw new Exception("Error accessing registry.");
-                            }
-                            key.SetValue("Name", logging_item.Sensor.DescriptionLong, RegistryValueKind.String);
-                            key.SetValue("Value", "0", RegistryValueKind.String);
-                        }
-
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-
+                AddHwinfoRegEntry();
+                
             }
 
             IsLogging = true;
@@ -223,7 +157,7 @@ namespace PMD
 
         public void WriteEntry()
         {
-
+            string text_str = String.Empty;
             string csv_str = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff") + ",";
             
             // Build output
@@ -231,15 +165,18 @@ namespace PMD
             {
                 if (logging_item.Enabled)
                 {
-                    string value = logging_item.Sensor.Value.ToString(culture_info);
 
-                    if (CsvMode)
-                    {
+                    string value = logging_item.Sensor.Value.ToString(logging_item.Sensor.Format, culture_info);
+
+                    if (WriteToTextFile) {
+                        text_str += value + Environment.NewLine;
+                    }
+
+                    if (LogToCsv) {
                         csv_str += value + ",";
                     }
 
-                    if (Hwinfo)
-                    {
+                    if (ExportToHwinfo) {
                         RegistryKey key = hwinfo_reg_key.OpenSubKey(logging_item.HwinfoKey, true);
                         key.SetValue("Value", value, RegistryValueKind.String);
                     }
@@ -247,14 +184,22 @@ namespace PMD
                 }
             }
 
-            // Write to file
-            if (CsvMode)
+            if(WriteToTextFile)
+            {
+                try
+                {
+                    File.WriteAllText(TextFilePath, text_str);
+                }
+                catch { }
+            }
+
+            if(LogToCsv)
             {
                 csv_str = csv_str.Substring(0, csv_str.Length - 1);
                 csv_str += Environment.NewLine;
                 try
                 {
-                    File.AppendAllText(FilePath, csv_str);
+                    File.AppendAllText(CsvFilePath, csv_str);
                 }
                 catch { }
             }
@@ -265,7 +210,7 @@ namespace PMD
         {
 
             // Remove registry values
-            if (Hwinfo)
+            if (ExportToHwinfo)
             {
                 RemoveHwinfoRegEntry();
             }
@@ -273,9 +218,78 @@ namespace PMD
             IsLogging = false;
         }
 
+        private void AddHwinfoRegEntry()
+        {
+            try
+            {
+
+                if (hwinfo_reg_key.OpenSubKey(DEVICE_NAME) != null)
+                {
+                    RemoveHwinfoRegEntry();
+                }
+
+                hwinfo_reg_key = hwinfo_reg_key.CreateSubKey(DEVICE_NAME);
+
+                if (hwinfo_reg_key == null)
+                {
+                    throw new Exception("Error accessing registry.");
+                }
+
+                int temp_idx = 0;
+                int current_idx = 0;
+                int volt_idx = 0;
+                int power_idx = 0;
+                int fan_idx = 0;
+                int usage_idx = 0;
+                foreach (LoggingItem logging_item in LoggingItemList)
+                {
+
+                    switch (logging_item.Sensor.Unit)
+                    {
+                        case "°C":
+                            logging_item.HwinfoKey = $"Temp{temp_idx++}"; break;
+                        case "A":
+                            logging_item.HwinfoKey = $"Current{current_idx++}"; break;
+                        case "V":
+                            logging_item.HwinfoKey = $"Volt{volt_idx++}"; break;
+                        case "W":
+                            logging_item.HwinfoKey = $"Power{power_idx++}"; break;
+                        case "RPM":
+                            logging_item.HwinfoKey = $"Fan{fan_idx++}"; break;
+                        case "%":
+                            logging_item.HwinfoKey = $"Usage{usage_idx++}"; break;
+                        default:
+                            logging_item.Enabled = false; break;
+                    }
+
+                    if (logging_item.Enabled)
+                    {
+                        RegistryKey key = hwinfo_reg_key.CreateSubKey(logging_item.HwinfoKey);
+                        if (key == null)
+                        {
+                            throw new Exception("Error accessing registry.");
+                        }
+                        key.SetValue("Name", logging_item.Sensor.DescriptionLong, RegistryValueKind.String);
+                        key.SetValue("Value", "0", RegistryValueKind.String);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
         private void RemoveHwinfoRegEntry()
         {
-            hwinfo_reg_key.DeleteSubKeyTree("");
+
+            try
+            {
+                hwinfo_reg_key.DeleteSubKeyTree("");
+                hwinfo_reg_key.Flush();
+            } catch { }
+
         }
     }
 }
